1868c382112a44d58d16480f45f772be58463e52
gaofeixmrs <724291658@qq.com>
2016-04-14 14:17:46 +0800

    基本页面

--- src/routes/test.js
+++ src/routes/test.js
@@ -2,13 +2,18 @@
 /**
 *@author gaofei <724291658@qq.com>
 **/
+import path from 'path';
 
 module.exports = function (done) {
 
- $.router.get('/',function (req, res, next) {
-   res.end(`现在是北京时间${new Date()}`);
- });
- 
- done();
- 
+  $.router.get('*', function (req, res, next) {
+    if (req.url.indexOf('/api/') !== 0 && req.url.indexOf('/build/') !== 0) {
+      res.sendFile(path.resolve(__dirname, '../../frontend/index.html'));
+    } else {
+      next();
+    }
+  });
+
+  done();
+
 };
