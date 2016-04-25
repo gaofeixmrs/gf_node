c06e17776ed1dd5e0446863705214d25eb2fedc8
gaofeixmrs <724291658@qq.com>
2016-04-25 14:54:04 +0800

    完成编辑功能、Markdown编辑器、完成注册功能

--- frontend/entry.js
+++ frontend/entry.js
@@ -8,6 +8,8 @@ import App from './App';
 import TopicDetail from './component/TopicDetail';
 import Login from './component/Login';
 import NewTopic from './component/NewTopic';
+import EditTopic from './component/EditTopic';
+import Signup from './component/Signup';
 
 const e = document.createElement('div');
 e.id = 'app';
@@ -17,8 +19,10 @@ ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <Route path="topic/:id" component={TopicDetail}/>
+      <Route path="topic/:id/edit" component={EditTopic}/>
       <Route path="new" component={NewTopic}/>
       <Route path="login" component={Login}/>
+      <Route path="signup" component={Signup}/>
     </Route>
   </Router>
 ), e);
